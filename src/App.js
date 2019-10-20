8347c2909e8b5047b36ffca50627918cf877315b
Carlos Gómez <carlos@MacBook-Pro-de-Carlos.local>
2019-10-20 13:44:02 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there, I am Carlos
+          Hi there, I am tatati
         </p>
         <a
           className="App-link"
